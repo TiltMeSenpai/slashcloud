16848aa062fcdd54fba8e633fcd2c165aced1dd2
Josh Koike <jk@tilting.me>
2021-09-12 12:41:13 -0700

    Clean up response logging

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,8 @@ use worker::*;
 mod utils;
 mod types;
 
-async fn handle(mut req: Request, env: Env) -> Result<Response> {
+#[event(fetch)]
+pub async fn main(mut req: Request, env: Env) -> Result<Response> {
     let body = req.bytes().await.unwrap();
     let ctx = utils::JsCtx::new();
     let key = match ctx.get_key(&env).await{
@@ -14,10 +15,4 @@ async fn handle(mut req: Request, env: Env) -> Result<Response> {
         Ok(()) => worker::Response::from_json(&types::interactions::InteractionResponse::Pong),
         Err(msg) => return worker::Response::error(msg, 401)
     }
-}
-#[event(fetch)]
-pub async fn main(req: Request, env: Env) -> Result<Response> {
-    let resp = handle(req, env).await;
-    console_log!("code: {:?}", resp);
-    resp
 }
