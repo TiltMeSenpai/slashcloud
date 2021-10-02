705c087f13a07c8d8883b68309b85052151a892e
Josh Koike <jk@tilting.me>
2021-10-02 10:13:05 -0700

    Re-export API types

--- src/discord/mod.rs
+++ src/discord/mod.rs
@@ -18,3 +18,17 @@ mod guild;
 
 #[cfg(feature = "channel")]
 mod channel;
+
+pub use snowflake::Snowflake;
+
+#[cfg(feature = "user")]
+pub use user::User;
+
+#[cfg(feature = "role")]
+pub use role::Role;
+
+#[cfg(feature = "emoji")]
+pub use emoji::Emoji;
+
+#[cfg(feature = "guild")]
+pub use guild::Guild;
